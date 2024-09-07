54604422ea05c5a0ffa2b19421631b4c7350d2a4
Priyanshu Maurya <94728532+Priyanshu-5257@users.noreply.github.com>
2024-09-08 00:13:08 +0530

    Update modeling_bert.py
    Removed NoisyGAte

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -467,28 +467,18 @@ class BertOutput(nn.Module):
         hidden_states = self.LayerNorm(hidden_states + input_tensor)
         return hidden_states
 
-class NoisyTopkRouter(nn.Module):
+class TopkRouter(nn.Module):
     def __init__(self, n_embed, num_experts, top_k):
-        super(NoisyTopkRouter, self).__init__()
+        super(TopkRouter, self).__init__()
         self.top_k = top_k
         #layer for router logits
         self.topkroute_linear = nn.Linear(n_embed, num_experts)
-        self.noise_linear =nn.Linear(n_embed, num_experts)
-
     
     def forward(self, mh_output):
         # mh_ouput is the output tensor from multihead self attention block
         logits = self.topkroute_linear(mh_output)
-
-        #Noise logits
-        noise_logits = self.noise_linear(mh_output)
-
-        #Adding scaled unit gaussian noise to the logits
-        noise = torch.randn_like(logits)*F.softplus(noise_logits)
-        noisy_logits = logits + noise
-
-        top_k_logits, indices = noisy_logits.topk(self.top_k, dim=-1)
-        zeros = torch.full_like(noisy_logits, float('-inf'))
+        top_k_logits, indices = logits.topk(self.top_k, dim=-1)
+        zeros = torch.full_like(logits, float('-inf'))
         sparse_logits = zeros.scatter(-1, indices, top_k_logits)
         router_output = F.softmax(sparse_logits, dim=-1)
         return router_output, indices
@@ -554,7 +544,7 @@ class BertLayer(nn.Module):
         
         self.moe = moe 
         if self.moe :
-            self.intermediate = SparseMoE(config,BertIntermediate(config), NoisyTopkRouter(config.hidden_size,5, 2))
+            self.intermediate = SparseMoE(config,BertIntermediate(config), TopkRouter(config.hidden_size,5, 2))
         if not self.moe:
             self.intermediate = BertIntermediate(config)
         self.output = BertOutput(config)
